d37262a7610a72da13fe4a6fea4a2cc4825a69de
gabrielvcb7 <gabriel.00000849430@unicap.br>
2024-03-19 11:55:38 -0300

    Exercicio C

---
+++ C/1186_-_Below_the_Secundary_Diagonal.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    char opcao;
+    scanf("%c", &opcao);
+    int n = 12;
+    float media = 0, soma = 0;
+    float matriz[12][12];
+            for (int i = 0; i < 12; ++i) {
+                for (int j = 0; j < 12; ++j) {
+                    scanf("%f", &matriz[i][j]);
+                }
+            }
+
+    switch (opcao) {
+        case 'S': 
+            for (int i = 0; i < 12; ++i) {
+                for (int j = 0; j < 12; ++j) {
+                    if (i + j > n - 1)
+                        soma += matriz[i][j];
+                }
+            }
+            printf("%.1f\n", soma);
+            break;
+        case 'M':
+            for (int i = 0; i < 12; ++i) {
+                for (int j = 0; j < 12; ++j) {
+                    if (i + j > n - 1)
+                        soma += matriz[i][j];
+                }
+            }
+            media = soma / 66;
+            printf("%.1f\n", media);
+            break;
+    }
+
+    return 0;
+}
